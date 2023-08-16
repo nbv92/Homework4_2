98f97f0250b57991a65c98c49d46f68781894465
Timofey <timofey.sharipov@gmail.com>
2023-08-16 06:40:20 +0300

    First_commit

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+public class Main {
+    public static void main(String[] args){
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+    }
+
+    public static void task1() {
+        System.out.println("Task1");
+    }
+
+    public static void task2() {
+        System.out.println("Task2");
+    }
+
+    public static void task3() {
+        System.out.println("Task3");
+    }
+
+    public static void task4() {
+        System.out.println("Task4");
+    }
+
+    public static void task5() {
+        System.out.println("Task5");
+    }
+
+    public static void task6() {
+        System.out.println("Task6");
+    }
+
+    public static void task7() {
+        System.out.println("Task7");
+    }
+
+    public static void task8() {
+        System.out.println("Task8");
+    }
+
+
